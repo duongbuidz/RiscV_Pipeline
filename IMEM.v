794dd03b084c8c00c314448087fc7a52748d4d1c
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2026-06-17 23:45:53 +0700

    no change
    Increased memory size to 1KB and updated comment for clarity.

--- IMEM.v
+++ IMEM.v
@@ -2,10 +2,10 @@ module IMEM (
     input      [31:0] PC_Out,
     output     [31:0] instruction
 );
-    reg [31:0] imem [0:255];    // 256 words = 1KB
+    reg [31:0] imem [0:1023];    // 256 words = 1KB
  
     initial begin
-        $readmemh("program.hex", imem);   // FIX: load từ file
+        $readmemh("program.hex", imem);   // flex with file hex name
     end
  
     assign instruction = imem[PC_Out[9:2]]; // word-addressed, combinational
