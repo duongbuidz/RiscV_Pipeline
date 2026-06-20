1f04029fce5f84af6688ccdce2be6e1a798dba52
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2026-06-20 16:17:17 +0700

    Add UART module
    Implement UART module with transmit functionality.

---
+++ uart.v
@@ -0,0 +1,54 @@
+module uart (
+  input clk,rst,
+  input [31:0] addr,
+  input [31:0] write_data,
+  input mem_write, 
+  input mem_read, 
+  input [31:0] cycle_counter,
+  output reg [31:0] read_data, 
+  output uart_tx
+);
+
+parameter UART_TX_DATA = 32'h10000000;
+parameter UART_TX_STATUS = 32'h10000004;
+parameter UART_CYCLE_COUNTER = 32'h10000008;
+
+wire tx_ready;
+reg [7:0] tx_data;
+reg tx_send;
+reg mem_write_d;
+
+uart_tx tx_core (
+  .clk,
+  .rst,
+  .data_in(tx_data),
+  .send(tx_send),
+  .tx(uart_tx),
+  .ready(tx_ready)
+);
+
+always @(posedge clk or posedge rst) begin
+  if(rst) begin
+    tx data <= 0;
+    tx send <= 0;
+  end else begin
+    tx send <= 1'b0;
+    mem_write_d <= mem write;
+    if(mem_write && !mem_write_d && addr == UART_TX_DATA && tx_ready) begin
+      tx_data = write_data[7:0];
+      tx_send <= 1'bl;
+    end
+  end
+end
+
+always @(*) begin
+  read data = 32'h0;
+  if(mem_read) begin
+    if(addr == UART_TX_STATUS)
+      read_data = {31'b0, tx_ready);
+    else if(addr == UART_CYCLE_COUNTER)
+  end
+  read_data = cycle_counter;
+end
+
+endmodule
