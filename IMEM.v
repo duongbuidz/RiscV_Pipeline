97a5dbb5db33596fdc2cfe885a8f23ed61671c3a
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2026-06-20 17:10:26 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -2,7 +2,7 @@ module IMEM (
     input      [31:0] PC_Out,
     output     [31:0] instruction
 );
-    reg [31:0] imem [0:1023];    // 256 words = 1KB
+    reg [31:0] imem [0:4095];    // 256 words = 1KB
  
     initial begin
         $readmemh("program.hex", imem);   // flex with file hex name
