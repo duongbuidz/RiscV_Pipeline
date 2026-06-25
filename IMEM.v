1ceab1e109c35010ee122f040169097b03726180
duongbuidz <135213865+duongbuidz@users.noreply.github.com>
2026-06-25 11:16:40 +0700

    Update IMEM.v

--- IMEM.v
+++ IMEM.v
@@ -2,11 +2,11 @@ module IMEM (
     input      [31:0] PC_Out,
     output     [31:0] instruction
 );
-    reg [31:0] imem [0:4095];    // 256 words = 1KB
+    reg [31:0] imem [0:8191];    // 256 words = 1KB
  
     initial begin
         $readmemh("program.hex", imem);   // flex with file hex name
     end
  
-    assign instruction = imem[PC_Out[13:2]]; // word-addressed, combinational
+    assign instruction = imem[PC_Out[14:2]]; // word-addressed, combinational
 endmodule
